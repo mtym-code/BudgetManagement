ff2756077c1130f7309d0997e5dd19de633f77ea
mtym-code <a4.mtym@gmail>
2026-06-08 10:57:43 +0900

    追加

---
+++ BudgetManagement/Views/Login.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+using System.Windows.Controls;
+using BudgetManagement.ViewModel; // 名前空間をプロジェクトに統一
+
+namespace BudgetManagement.Views
+{
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+
+            var vm = App.Services.GetService<LoginViewModel>();
+            DataContext = vm;
+
+            vm.LoginSucceeded += OnLoginSucceeded;
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = (LoginViewModel)DataContext;
+            vm.Password = PasswordBox.Password;
+        }
+
+        private void OnLoginSucceeded()
+        {
+            // 【変更】元々あった UserCsvImport への遷移処理は、ここからメニュー画面へと移植しました。
+            // ログイン成功時はまずハブとなるメニュー画面を開きます。
+            this.NavigationService.Navigate(new MenuPage());
+        }
+    }
+}
---
+++ BudgetManagement/Views/MenuPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection; // App.Services を使うために追加
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace BudgetManagement.Views
+{
+    public partial class MenuPage : Page
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 【新規追加】Login.xaml.cs から引っ越してきた遷移ロジック
+        /// メニューのボタンが押されたときに、元々の機能画面（UserCsvImport）を起動します。
+        /// </summary>
+        private void UserCsvImport_Click(object sender, RoutedEventArgs e)
+        {
+            // 元々 Login.xaml.cs に書いてあったコードをそのままここで実行します
+            this.NavigationService.Navigate(
+                App.Services.GetService<UserCsvImport>()
+            );
+        }
+
+        private void TabBudget_Click(object sender, MouseButtonEventArgs e)
+        {
+            BudgetPanel.Visibility = Visibility.Visible;
+            MaintenancePanel.Visibility = Visibility.Collapsed;
+
+            TabBudget.Background = Brushes.White;
+            TabBudget.BorderBrush = new SolidColorBrush(Color.FromRgb(99, 102, 241));
+
+            TabMaintenance.Background = new SolidColorBrush(Color.FromRgb(229, 231, 235));
+            TabMaintenance.BorderBrush = new SolidColorBrush(Color.FromRgb(229, 231, 235));
+        }
+
+        private void TabMaintenance_Click(object sender, MouseButtonEventArgs e)
+        {
+            BudgetPanel.Visibility = Visibility.Collapsed;
+            MaintenancePanel.Visibility = Visibility.Visible;
+
+            TabMaintenance.Background = Brushes.White;
+            TabMaintenance.BorderBrush = new SolidColorBrush(Color.FromRgb(99, 102, 241));
+
+            TabBudget.Background = new SolidColorBrush(Color.FromRgb(229, 231, 235));
+            TabBudget.BorderBrush = new SolidColorBrush(Color.FromRgb(229, 231, 235));
+        }
+    }
+}
